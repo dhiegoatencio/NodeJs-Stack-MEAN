45283688069027eab816af11032815579a6663f0
unknown <dhiegoatencio>
2014-08-11 19:32:27 -0300

    corrigido response para o post

--- app/routes.js
+++ app/routes.js
@@ -1,37 +1,36 @@
 module.exports = function(app, mongoose) {
-
 	// server routes ===========================================================
 	// handle things like api calls
 	// authentication routes
-
 	// sample api route
 
 	app.post('/api/nerds', function(req, res){
 		var Nerd = mongoose.model('nerds');
 		var nerd = new Nerd(req.body);
-		console.log("Salvando nerd");
 
 		nerd.save(function(err){
-			console.log(req.body);
-		});		
+			if (!err) {
+				res.send(200); //gravou corretamente
+			} else {
+				res.send(404); //ocorreu algum erro
+			};
+		});
 	});
-
 
 	app.get('/api/nerds', function(req, res) {
 		mongoose.model('nerds').find(function(err, nerds){
 			res.json(nerds);
 		});
-        /*res.json([{"nome": "Dhiego" , "sobrenome":"Hendrix"}, 
-		          {"nome": "Nhé"     , "sobrenome": "Polar"  }]);*/
 	});
 
 	// route to handle creating (app.post)
 	// route to handle delete (app.delete)
+
+
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
 	app.get('*', function(req, res) {
 		res.sendfile('./public/views/index.html'); // load our public/index.html file
 	});
-
 };
--- public/js/controllers/NerdCtrl.js
+++ public/js/controllers/NerdCtrl.js
@@ -3,25 +3,19 @@ angular.module('NerdCtrl', []).controller('NerdController', ['$scope', 'Nerd', f
 	$scope.tagline = 'Esta mensagem NERD, está vindo do arquivo NerdController.js';
     $scope.nerd = {};
 
-
     Nerd.get().success(function(data, status, headers, config) {
         $scope.nerds = data;
     }).error(function(data, status, headers, config) {
         $scope.tagline = 'Houve um erro ao carregar a lista de NERDS';
-        
     });;
-
 
     $scope.salvar = function() {
     	Nerd.create($scope.nerd).success(function(data, status, headers, config) {
-            alert("Success");
-            //});function(){
-    	//	$scope.nerds.unshift($scope.nerd);
-          //  $scope.nerd = "";
-         //   $scope.$digest();
+    		$scope.nerds.unshift($scope.nerd);
+            $scope.nerd = "";
     	}).error(function(){
             $scope.tagline = "Erro ao gravar";
         });
-    }
+    };
 
 }]);
