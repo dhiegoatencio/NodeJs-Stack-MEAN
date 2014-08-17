6fe8d32510b8c97560c4653e3f197e5faafc0b1e
dhiegoatencio <dhiegoatencio@users.noreply.github.com>
2014-08-17 16:37:27 -0300

    adicionado rota para login, e controle fixo de atenticacao temporaria.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,10 +4,31 @@ module.exports = function(app) {
 	// authentication routes
 	// sample api route
 
+	var isLoged = false;
 
+	app.get('*', function(req, res){
+		if (isLoged) {
+            res.sendfile('./public/views/index.html'); // load our public/index.html file
+		} else if (req.body.user == 'user' &&
+			       req.body.password == "123") {
+			isLoged = true;
+		} else {
+			res.sendfile('./public/views/login.html');
+		};
+	});
+
+	app.post('/api/login', function(req, res){
+	    if (req.body.user == 'user' && req.body.password == "123") {
+			isLoged = true;
+            res.sendfile('./public/views/index.html'); // load our public/index.html file
+		} else {
+			res.sendfile('./public/views/login.html');
+		};
+	});
+/*
 	// frontend routes =========================================================
 	// route to handle all angular requests
 	app.get('*', function(req, res) {
 		res.sendfile('./public/views/index.html'); // load our public/index.html file
-	});
+	});*/
 };
